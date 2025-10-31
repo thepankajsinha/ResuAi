925a8184fd186a872a04a111cd1df5fd8d7b6602
Pankaj Kumar <136917326+thepankajsinha@users.noreply.github.com>
2025-10-31 16:01:31 +0530

    Fix formatting in axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://resuai-ylvj.onrender.com/",
+  baseURL: "https://resuai-ylvj.onrender.com",
   withCredentials: true, // Set to true if using cookies
 });
 
 export default api;
+
 
